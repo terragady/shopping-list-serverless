9116e4bf898a2718f3be8018eca69b4521d9b79e
Marcin Michalik <terragady@gmail.com>
2023-02-03 18:25:24 +0100

    po

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as Realm from 'realm-web';
+import styled from 'styled-components';
+
+const Login = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: red;
+`;
 
 function App() {
+  const [token, setToken] = React.useState<string | null>(null);
+  const app = new Realm.App({ id: 'shopping-list-serverless-fqtdh' });
+  // The redirect URI should be on the same domain as this app and
+  // specified in the auth provider configuration.
+  const redirectUri = 'localhost:3000/hello';
+
+  // Calling logIn() opens a Facebook authentication screen in a new window.
+  const login = () => {
+    app.logIn(Realm.Credentials.facebook(redirectUri)).then((user) => {
+      // The logIn() promise will not resolve until you call `handleAuthRedirect()`
+      // from the new window after the user has successfully authenticated.
+      console.log(`Logged in with id: ${user.id}`);
+    });
+    // When the user is redirected back to your app, handle the redirect to
+    // save the user's access token and close the redirect window. This
+    // returns focus to the original application window and automatically
+    // logs the user in.
+  };
+  const login2 = () => {
+    app.logIn(Realm.Credentials.emailPassword("terragady@gmail.com", "test4321")).then((user) => {
+      // The logIn() promise will not resolve until you call `handleAuthRedirect()`
+      // from the new window after the user has successfully authenticated.
+      console.log(`Logged in with id: ${JSON.stringify(user)}`);
+    });
+    // When the user is redirected back to your app, handle the redirect to
+    // save the user's access token and close the redirect window. This
+    // returns focus to the original application window and automatically
+    // logs the user in.
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <Login onClick={() => login()}></Login>
+        <Login onClick={() => login2()}></Login>
       </header>
     </div>
   );
