b74c396ea00523b4336b14046f45b34579ecee7a
Marcin Michalik <terragady@gmail.com>
2023-02-03 18:51:53 +0100

    working

--- src/App.tsx
+++ src/App.tsx
@@ -13,19 +13,21 @@ function App() {
   const app = new Realm.App({ id: 'shopping-list-serverless-fqtdh' });
   // The redirect URI should be on the same domain as this app and
   // specified in the auth provider configuration.
-  const redirectUri = 'localhost:3000/hello';
+  const redirectUri = 'http://localhost:3000';
+  Realm.handleAuthRedirect();
 
   // Calling logIn() opens a Facebook authentication screen in a new window.
   const login = () => {
     app.logIn(Realm.Credentials.facebook(redirectUri)).then((user) => {
       // The logIn() promise will not resolve until you call `handleAuthRedirect()`
       // from the new window after the user has successfully authenticated.
-      console.log(`Logged in with id: ${user.id}`);
+      console.log(`Logged in with id: ${JSON.stringify(user)}`);
     });
     // When the user is redirected back to your app, handle the redirect to
     // save the user's access token and close the redirect window. This
     // returns focus to the original application window and automatically
     // logs the user in.
+    Realm.handleAuthRedirect();
   };
   const login2 = () => {
     app.logIn(Realm.Credentials.emailPassword("terragady@gmail.com", "test4321")).then((user) => {
