ebfdd855293c12679d70b9cee6a85a53aeaae1c3
Marcin Michalik <terragady@gmail.com>
2023-02-07 19:42:15 +0100

    fix cache

--- src/modules/FromPoland.tsx
+++ src/modules/FromPoland.tsx
@@ -5,7 +5,7 @@ import CircleSpinner, { LoaderWrapper } from 'components/loader/CircleSpinner'
 
 function FromPoland() {
   const { data, loading, error } = useFromPoland({
-    fetchPolicy: 'cache-and-network'
+    fetchPolicy: 'cache-and-network',
   })
   const [deleteFromPoland] = useDeleteFromPoland({
     update: (cache, { data }) => {
@@ -13,14 +13,12 @@ function FromPoland() {
         cache.modify({
           fields: {
             zPolskis(existingFromPoland = [], { readField }) {
-              return existingFromPoland.filter(
-                (fromPolandRef: any) => data.deleteOneZPolski?._id !== readField('_id', fromPolandRef)
-              )
+              return existingFromPoland.filter((fromPolandRef: any) => data.deleteOneZPolski?._id !== readField('_id', fromPolandRef))
             },
           },
         })
       }
-    }
+    },
   })
   const [addFromPoland] = useAddFromPoland({
     update: (cache, { data }) => {
@@ -28,25 +26,48 @@ function FromPoland() {
         cache.modify({
           fields: {
             zPolskis(existingFromPoland = []) {
+              console.log('existingFromPoland', existingFromPoland)
+              console.log(data.insertOneZPolski)
+              console.log([...existingFromPoland, data.insertOneZPolski])
               return [...existingFromPoland, data.insertOneZPolski]
+            },
+          },
+        })
+      }
+    },
+  })
+  const [updateFromPoland] = useUpdateFromPoland({
+    update: (cache, { data }) => {
+      if (data?.updateOneZPolski) {
+        cache.modify({
+          fields: {
+            zPolskis(existingFromPoland = [], { readField }) {
+              return existingFromPoland.map((fromPolandRef: any) => {
+                if (data.updateOneZPolski?._id === readField('_id', fromPolandRef)) {
+                  return data.updateOneZPolski
+                }
+                return fromPolandRef
+              })
             },
           },
         })
       }
     }
   })
-  const [updateFromPoland] = useUpdateFromPoland()
   const [deleteManyFromPolands] = useDeleteManyFromPolands({
-    refetchQueries: ['FromPolands']
+    refetchQueries: ['FromPoland'],
   })
 
-  if (error) return (<div>Error: {error.message}</div>)
+  if (error) return <div>Error: {error.message}</div>
 
-  if (!data && loading) return <LoaderWrapper><CircleSpinner size={40}/></LoaderWrapper>
-  if (!data) return null;
-  return (
-    <List data={data?.zPolskis} onDelete={deleteFromPoland} onAdd={addFromPoland} onPatch={updateFromPoland} onDeleteMany={deleteManyFromPolands}/>
-  )
+  if (!data && loading)
+    return (
+      <LoaderWrapper>
+        <CircleSpinner size={40} />
+      </LoaderWrapper>
+    )
+  if (!data) return null
+  return <List data={data?.zPolskis} onDelete={deleteFromPoland} onAdd={addFromPoland} onPatch={updateFromPoland} onDeleteMany={deleteManyFromPolands} />
 }
 
 export default FromPoland
